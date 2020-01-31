a28df607ea26383be6ed61c553deddf08dccc332
raphael2605 <raphael_hd@outlook.com>
2020-01-30 22:06:40 -0300

    init

---
+++ scripts/app.js
@@ -0,0 +1,46 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+
+const updateUI = (data) => {
+
+    //const cityDets = data.cityDets;
+    //const weather = data.weather;
+    
+    //igual ao de cima
+    const { cityDets, weather } = data;
+
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+};
+const updateCity = async (city) => {
+
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key); 
+
+    console.log(cityDets)
+    return {cityDets, weather};
+
+};
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+});
---
+++ scripts/forecast.js
@@ -0,0 +1,23 @@
+const key = 'WFeRoTnQbskEM1T6X8nzt0kx8kVylWPu';
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0]
+};
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
